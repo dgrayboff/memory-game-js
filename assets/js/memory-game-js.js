070a899c4274fb62cb8e4ff2666f9abbcf3ba0f0
David <dgrayboff@gmail.com>
2016-06-03 14:38:55 -0400

    added functionality for flip, in process of comparing card icons

--- assets/js/memory-game-js.js
+++ assets/js/memory-game-js.js
@@ -1,8 +1,35 @@
-var el = $('.card');
+// var el = $('.card');
 
-$('.card').each(function(i, el) {
   $('.card').on('click', function(){
-    $('.card').addClass('flip')
+    $(this).addClass('flip');
+    var flipped = $('.flip');
+    if(flipped.length === 2) { // recognizes the second click and flip
+      var firstCard = flipped.first();
+      var secondCard = flipped.last();
+      // var aa = $('i').firstCard;
+      // var bb = $('i').secondCard;
+      // var array = $('.')
+      // if (firstCard.hasClass('.i') == secondCard.hasClass('.i')) {
+      // var array = $('i').attr('class').split('')[1];
+      // remember you switched first class name in first two cards in HTML
+      if ($('i').attr('id') == $('i').attr('id')) {
+        console.log("it matches!");
+      }
+        else {
+          console.log("doesn't match")
+        }
+
+    }
+
+    // $('.card').toggleClass('back');
+    // $('.card-icon').css('animation', 'hideicon');
+    // $('.card-icon').toggleClass();
+    // if()
+    // if($('.i').first().is()
+    //   $('.card').removeClass('face');
     console.log("hello");
   })
-})
+
+  // if($('.card').toggleClass('flip') === true) {
+  //    $('.card-icon').toggleClass('front');
+  // }
